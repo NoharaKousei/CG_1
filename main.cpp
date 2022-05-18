a5273e4032ea18ef44ddc45316414707f0da9483
nohara_kousei <k021g1159@g.neec.ac.jp>
2022-05-18 12:06:28 +0900

    03_04

--- main.cpp
+++ main.cpp
@@ -276,14 +276,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//頂点データ
 	XMFLOAT3 vertices[] = {
 		
-		{-0.5f,-0.5f,0.0f},//xが-で左 yが-で下 左下
-		{+0.5f,-0.5f,0.0f},//xが+で右 yが-で下 右下
-		{-0.5f,0.5f,0.0f},//左中
-		{+0.5f,0.5f,0.0f},//右中
-		{-0.5f,+0.5f,0.0f},//xが-で左 yが+で上 左上
-		{+0.5f,+0.5f,0.0f} //xが+で右 yが+で上 右上
+		{-0.5f,-0.5f,0.0f},//xが-で左 yが-で下 左下 インデックス0
+		{-0.5f,+0.5f,0.0f},//xが-で左 yが+で上 左上 インデックス1
+		{+0.5f,-0.5f,0.0f},//xが+で右 yが-で下 右下 インデックス2
+		{+0.5f,+0.5f,0.0f} //xが+で右 yが+で上 右上 インデックス3
 
 	};
+
+	//インデックスデータ
+	uint16_t indices[] =
+	{
+		0,1,2,   //三角形1つ目
+		1,2,3,   //三角形2つ目
+	};
+
 	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ + 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
 
@@ -399,6 +405,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		},//(1行で書いたほうが見やすい)
 		//座標以外に色、テクスチャUVなどを渡す場合はさらに続ける
 	};
+
+	//インデックスデータ全体のサイズ
+	UINT sizeIB = static_cast<UINT>(sizeof(uint16_t) * _countof(indices));
+	//リソース設定
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeIB;  //インデックス情報が入る分のサイズ
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	//インデックスバッファの生成
+	ID3D12Resource* indexBuff = nullptr;
+	result = device->CreateCommittedResource(
+		&heapProp,  //ヒープ設定
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc,  //リソース設定
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&indexBuff));
+
+	//インデックスバッファをマッピング
+	uint16_t* indexMap = nullptr;
+	result = indexBuff->Map(0, nullptr, (void**)&indexMap);
+	//全インデックスに対して
+	for (int i = 0; i < _countof(indices); i++)
+	{
+		indexMap[i] = indices[i];  //インデックスをコピー
+
+	}
+	//マッピング解除
+	indexBuff->Unmap(0, nullptr);
+
+	//インデックスバッファビューの作成
+	D3D12_INDEX_BUFFER_VIEW ibView{};
+	ibView.BufferLocation = indexBuff->GetGPUVirtualAddress();
+	ibView.Format = DXGI_FORMAT_R16_UINT;
+	ibView.SizeInBytes = sizeIB;
 
 	//グラフィックスパイプライン設定
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
@@ -547,6 +592,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
 		}
 
+		//インデックスバッファビューの設定コマンド
+		commandList->IASetIndexBuffer(&ibView);
+
 		//4.描画コマンドここから
 		//ビューポート設定コマンド
 		D3D12_VIEWPORT viewport{};           
@@ -582,7 +630,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		commandList->SetGraphicsRootConstantBufferView(0, constBuffMaterial->GetGPUVirtualAddress());
 
 		//描画コマンド
-		commandList->DrawInstanced(_countof(vertices), 1, 0, 0);  //全ての頂点を使って描画
+		commandList->DrawIndexedInstanced(_countof(vertices), 1, 0, 0, 0);  //全ての頂点を使って描画
 
 
 		//4.描画コマンドここまで
